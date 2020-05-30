035856297a0d96f792d968252a0beec79d5d51c5
Jomark Pangan <jomarkrazonpangan@gmail.com>
2020-05-30 14:32:50 +0800

    Refactored a lot of code/logic

--- script.js
+++ script.js
@@ -1,99 +1,84 @@
 
-let choices = ["Rock", "Paper", "Scissors"];
 
+const playerButtons = document.querySelectorAll(".player-container > button");
+const gameBoard = document.querySelector(".game-board >.result-display");
+const playerMove = document.querySelector("#player-move");
+const computerMove = document.querySelector("#computer-move");
 let playerScore = 0;
 let computerScore = 0;
-const buttons = document.querySelectorAll("button");
-const container = document.querySelector(".container");
-const results = document.querySelector(".results");
 
-buttons.forEach((button) => {
 
-    button.addEventListener('click', (e) => {
+const hands = ["rock", "paper", "scissors"];
 
-        let playerSelection = button.className;
 
-        console.log(playerSelection);
 
-        let computersSelection = getComputersSelection();
 
-        playRound(playerSelection, computersSelection);
-        console.log("player score: " + playerScore + "\nComputer Score: " + computerScore);
-        const displayResult = document.createElement('div');
-        displayResult.classList.add("displayResults");
-        displayResult.textContent = "player Score: " + playerScore
-            + "\nComputer Score: " + computerScore;
+const computerPlay = () => Math.floor(Math.random() * 3);
 
-        results.appendChild(displayResult);
 
-        if (playerScore >= 5) {
 
-            alert("You won the game! congratulations!")
-            playerScore = 0;
-            computerScore = 0;
-            while (results.firstChild) {
-                results.removeChild(results.lastChild);
-            }
-        } else if (computerScore >= 5) {
-            alert("You lost the game, Computer Wins!!")
-            playerScore = 0;
-            computerScore = 0;
-            while (results.firstChild) {
-                results.removeChild(results.lastChild);
-            }
-        }
 
+const getPlayersHands = (playerSelection, computerSelection) => {
+
+    const computersHand = hands[computerSelection];
+    playerMove.innerText = playerSelection;
+    computerMove.innerText = computersHand;
+    console.log(`Your Hand: ${playerSelection}\nOpponents: ${computersHand}`);
+
+    let result = "";
+
+    if (playerSelection == computersHand) {
+
+        result = "You picked the same hand!"
+    }
+
+    else if (playerSelection == "rock" && computersHand == "scissors" ||
+        playerSelection == "paper" && computersHand == "rock" ||
+        playerSelection == "scissors" && computersHand == "paper") {
+        result = `${playerSelection.toUpperCase()} beats ${computersHand.toUpperCase()}. You Win!`;
+        playerScore += 1;
+    }
+    else {
+        result = `${computersHand.toUpperCase()} beats ${playerSelection.toUpperCase()}. You Lost!`;
+        computerScore += 1;
+    }
+
+    console.log(`${result}\nPlayer Score: ${playerScore}\nComputer Score: ${computerScore}`);
+    showResult(result)    
+
+};
+
+const showResult = (res) => {
+
+    gameBoard.innerText = res;
+    
+    if(computerScore == 5){
+        gameBoard.innerText
+    }
+
+
+}
+
+const playRound = (playerHand) => {
+
+    getPlayersHands(playerHand,computerPlay());
+    let getComputerMove = computerPlay();
+
+  
+
+
+}
+
+playerButtons.forEach(hand => {
+
+    hand.addEventListener("click", function (e) {
+
+
+        let playerHand = e.target.innerText.toLowerCase();
+        playRound(playerHand);
 
     });
 });
 
-function getComputersSelection() {
-
-    let randomNumber = Math.floor((Math.random() * 3));
-    return choices[randomNumber];
-}
-
-function playRound(playersSelection, computersSelection) {
-
-    if (playersSelection == "Rock" && computersSelection == "Rock") {
-        alert("Draw!");
-    }
-    else if (playersSelection == "Paper" && computersSelection == "Rock") {
-        alert("You Win!");
-        playerScore += 1;
-    }
-    else if (playersSelection == "Scissors" && computersSelection == "Rock") {
-        alert("You Lost!");
-        computerScore += 1;
-    }
-
-    else if (playersSelection == "Rock" && computersSelection == "Paper") {
-        alert("You Lost!");
-        computerScore += 1;
-    }
-    else if (playersSelection == "Paper" && computersSelection == "Paper") {
-        alert("Draw!");
-
-    }
-    else if (playersSelection == "Scissors" && computersSelection == "Paper") {
-        alert("You Win!");
-        playerScore += 1;
-    }
-
-    else if (playersSelection == "Rock" && computersSelection == "Scissors") {
-        alert("You Win!");
-        playerScore += 1;
-    }
-    else if (playersSelection == "Paper" && computersSelection == "Scissors") {
-        alert("You Lost!");
-        computerScore += 1;
-
-    }
-    else if (playersSelection == "Scissors" && computersSelection == "Scissors") {
-        alert("Draw!");
-
-    }
 
 
-
-}
